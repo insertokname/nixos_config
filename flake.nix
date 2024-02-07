3152d9cc1d3a0f4dcb00e2b18f443b7f99f72bb2
insertokname <insertokname@gmail.com>
2024-02-07 14:07:05 +0000

    functioning config flake

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     lib = nixpkgs.lib;
   in{
     nixosConfigurations = {
-      nixos-tutorial = lib.nixosSystem{
+      nixos-fekete = lib.nixosSystem{
         system = "x86_64-linux";
 	modules = [ ./configuration.nix];
       };
