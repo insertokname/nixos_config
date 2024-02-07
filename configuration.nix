fd2c2e1208599bdf5333138c929a816097a59853
insertokname <insertokname@gmail.com>
2024-02-07 13:50:50 +0000

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,176 @@
+{ config, pkgs, ... }:
+
+{
+
+  imports = [
+    /etc/nixos/hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = true;
+
+  hardware.opengl={
+     enable = true;
+     driSupport = true;
+     driSupport32Bit = true;
+  };
+
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [];
+      allowedUDPPorts = [];
+    };
+    hostName = "nixos-fekete";
+    networkmanager.enable = true;
+  };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ro_RO.UTF-8";
+    LC_IDENTIFICATION = "ro_RO.UTF-8";
+    LC_MEASUREMENT = "ro_RO.UTF-8";
+    LC_MONETARY = "ro_RO.UTF-8";
+    LC_NAME = "ro_RO.UTF-8";
+    LC_NUMERIC = "ro_RO.UTF-8";
+    LC_PAPER = "ro_RO.UTF-8";
+    LC_TELEPHONE = "ro_RO.UTF-8";
+    LC_TIME = "ro_RO.UTF-8";
+    LC_CTYPE="en_US.utf8"; # required by dmenu don't change this
+  };
+
+  sound.enable = true;
+  time.hardwareClockInLocalTime = true;
+
+  services = {
+    xserver = {
+      layout = "us";
+      xkbVariant = "";
+      enable = true;
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          i3status
+        ];
+      };
+      desktopManager = {
+        xterm.enable = false;
+        xfce = {
+          enable = true;
+          noDesktop = true;
+          enableXfwm = false;
+        };
+      };
+      displayManager = {
+        lightdm.enable = true;
+        defaultSession = "xfce+i3";
+      };
+    };
+    gvfs.enable = true;
+    gnome.gnome-keyring.enable = true;
+    blueman.enable = true;
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+  
+  hardware.nvidia = {
+     modesetting.enable = true;
+
+     powerManagement.enable = false;
+     powerManagement.finegrained = false;
+     
+     open = false;
+
+     # Enable the Nvidia settings menu,
+     # accessible via `nvidia-settings`.
+     nvidiaSettings = true;
+
+     package = config.boot.kernelPackages.nvidiaPackages.stable;
+};
+
+  nix.settings.experimental-features = [ "nix-command" "flakes"];
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      pulseaudio = true;
+    };
+  };
+
+  users.users.fekete= {
+    isNormalUser = true;
+    description = "fekete";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      brave
+      xarchiver
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    alacritty
+    dmenu
+    git
+    gnome.gnome-keyring
+    nerdfonts
+    networkmanagerapplet
+    nitrogen
+    pasystray
+    picom
+    polkit_gnome
+    pulseaudioFull
+    rofi
+    neovim
+    unrar
+    unzip
+    firefox
+    python3
+    gcc
+    vscode
+    fish
+    gh #github cli
+  ];
+
+  programs = {
+    thunar.enable = true;
+    dconf.enable = true;
+  };
+
+  security = {
+    polkit.enable = true;
+    rtkit.enable = true;
+  };
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+  
+  hardware = {
+    bluetooth.enable = true;
+  };
+
+  system.stateVersion = "23.05";
+}
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "OS config";
+
+  inputs = {
+    nixpgs.url = "github:NixOs/nixpkgs/nixos-23.05"
+  };
+
+  outputs = { self, nixpkgs, ...}:
+  let
+    lib = nixpkgs.lib;
+  in{
+    nixosConfigurations = {
+      nixos-tutorial = lib.nixosSystem{
+        system = "x86_64-linux";
+	modules = [ ./configuration.nix];
+      };
+    };
+  };
+}
