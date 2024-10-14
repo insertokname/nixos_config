b1693aadd563de1e0e987739aa90892b9feeb697
insertokername <insertokname@gmail.com>
2024-10-14 23:43:48 +0300

    installed zoxide

--- home-manager/common/load_common.nix
+++ home-manager/common/load_common.nix
@@ -1,4 +1,9 @@
 { config, pkgs, ... }: {
-  imports =
-    [ ./alacritty.nix ./fish.nix ./i3_layout/load_i3_layout.nix ./vs_code.nix ];
+  imports = [
+    ./alacritty.nix
+    ./fish.nix
+    ./i3_layout/load_i3_layout.nix
+    ./vs_code.nix
+    ./zoxide.nix
+  ];
 }
