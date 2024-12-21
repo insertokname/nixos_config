a13c2da88cd1ba9e5e0881c5a7282918ce98d2e0
insertokername <111150085+insertokername@users.noreply.github.com>
2024-12-21 23:23:30 +0200

    even faster rclone sync

--- hardware/desktop/services/rclone.nix
+++ hardware/desktop/services/rclone.nix
@@ -7,7 +7,7 @@
     serviceConfig = {
       Type = "simple";
       ExecStart =
-        "${pkgs.rclone}/bin/rclone mount --config=/home/fekete/.config/rclone/rclone.conf obi-remote: /mnt/gdrive --buffer-size 32M --allow-other --dir-cache-time 300h --poll-interval 1m --max-read-ahead 200M";
+        "${pkgs.rclone}/bin/rclone mount --config=/home/fekete/.config/rclone/rclone.conf obi-remote: /mnt/gdrive --async-read --buffer-size 32M --allow-other --dir-cache-time 300h --poll-interval 1m --max-read-ahead 200M --transfers 24 --drive-chunk-size=32M --cache-chunk-total-size 2G --cache-chunk-size 16M --cache-workers=24 --buffer-size=32M --vfs-cache-mode full --vfs-read-chunk-size 32M --vfs-cache-max-age 1h";
       ExecStop = "${pkgs.util-linux}/bin/umount -u /mnt/gdrive";
       Restart = "always";
       RestartSec = 10;
